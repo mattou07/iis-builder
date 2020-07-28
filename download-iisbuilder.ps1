e6f3fded6196508280d5ba15956a504416dd99c3
Paul Seal <prjseal@gmail.com>
2020-07-28 09:52:39 +0100

    Create download-iisbuilder.ps1

---
+++ download-iisbuilder.ps1
@@ -0,0 +1,2 @@
+(new-object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/mattou07/iis-builder/master/IIS-Builder.ps1','IIS-Builder.ps1')
+(new-object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/mattou07/iis-builder/master/iis-config.json','iis-config.json')
